15a436081db2e46caf5df3ee56571d5713c079a3
Prehook727 <chenyizhen2025@outlook.com>
2026-04-15 20:06:59 +0800

    add table and node.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-ÿþ
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname, '/')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/home.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Press Ctrl+C to quit.`);
+});
